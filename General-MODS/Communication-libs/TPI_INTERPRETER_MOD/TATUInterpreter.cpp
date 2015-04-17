584ad07bf9b2f1328f8610e8ed605abe5aca29b0
jefersonla <oi_je@hotmail.com>
2015-04-17 00:45:38 -0300

    Corrigido alguns erros removido a FLAG ERROR do objeto (o metodo parse retorna true se executado corretamente). Após a execução do parse o objeto PAYLOAD se tornará uma string com a variavel e a propriedade separadas por um |

--- General-MODS/Communication-libs/TPI_INTERPRETER_MOD/TATUInterpreter.cpp
+++ General-MODS/Communication-libs/TPI_INTERPRETER_MOD/TATUInterpreter.cpp
@@ -1,10 +1,8 @@
 #include "TATUInterpreter.h"
 #include <stdint.h>
 
-#define HASH_DJB(START, LEN, INPUT, OUTPUT) for(i = START; i < LEN; i++){ OUTPUT = ((OUTPUT << 5) + OUTPUT) + INPUT[i]; }
-
 bool TATUInterpreter::parse(unsigned char *string, uint8_t length){
-    uint8_t i;
+    uint8_t i, j;
     str_hash = 5381;
 
     switch(string[0]){
@@ -14,6 +12,7 @@ bool TATUInterpreter::parse(unsigned char *string, uint8_t length){
             if(string[4] == CODE_INFO){
                 cmd.OBJ.CODE = TATU_CODE_INFO;
                 HASH_DJB(9, length, string, str_hash);
+                j = i; VAR_COPY(j, length, string);
             }
             else if(string[4] == CODE_STATE){
                 cmd.OBJ.CODE = TATU_CODE_STATE;
@@ -22,7 +21,6 @@ bool TATUInterpreter::parse(unsigned char *string, uint8_t length){
             }
             else break;
 
-            ERROR = false;
             return true;
         case COMMAND_GET:
             cmd.OBJ.TYPE = TATU_GET;
@@ -40,13 +38,13 @@ bool TATUInterpreter::parse(unsigned char *string, uint8_t length){
             }
             else break;
 
-            ERROR = false;
             return true;
         case COMMAND_EDIT:
             cmd.OBJ.TYPE = TATU_EDIT;
             if(string[5] == CODE_INFO){
                 cmd.OBJ.CODE = TATU_CODE_INFO;
                 HASH_DJB(10, length, string, str_hash);
+                j = i; VAR_COPY(j, length, string);
             }
             else if(string[5] == CODE_STATE){
                 cmd.OBJ.CODE = TATU_CODE_STATE;
@@ -55,18 +53,11 @@ bool TATUInterpreter::parse(unsigned char *string, uint8_t length){
             }
             else break;
 
-            ERROR = false;
             return true;
         case COMMAND_POST:
             cmd.OBJ.TYPE = TATU_POST;
-            ERROR = false;
             return true;
     }
 
-    ERROR = true;
     return false;
-}
-
-bool TATUInterpreter::getERROR(){
-    return ERROR;
 }
--- General-MODS/Communication-libs/TPI_INTERPRETER_MOD/TATUInterpreter.h
+++ General-MODS/Communication-libs/TPI_INTERPRETER_MOD/TATUInterpreter.h
@@ -3,25 +3,35 @@
 
 #include <stdint.h>
 
+// TATU Protocol available commands
 #define TATU_SET    0
 #define TATU_GET    1
 #define TATU_POST   2
 #define TATU_EDIT   3
 
+// TATU Protocol available properties
 #define TATU_CODE_ALL   0
 #define TATU_CODE_INFO  1
 #define TATU_CODE_PROP  2
 #define TATU_CODE_STATE 3
 
+// Char that represents the TATU Protocol commands
 #define CODE_ALL   'A'
 #define CODE_INFO  'I'
 #define CODE_PROP  'P'
 #define CODE_STATE 'S'
 
+// Char that represents the TATU Protocol properties
 #define COMMAND_SET  'S'
 #define COMMAND_GET  'G'
 #define COMMAND_POST 'P'
 #define COMMAND_EDIT 'E'
+
+// DJB Hash macro
+#define HASH_DJB(START, LEN, INPUT, OUTPUT) for(i = START; INPUT[i] != ' '; i++){ OUTPUT = ((OUTPUT << 5) + OUTPUT) + INPUT[i]; }
+// Copy property to begin of the payload array
+#define VAR_COPY(START, LEN, VECTOR) for(i = START; i < LEN; i++){ VECTOR[i-START] = VECTOR[i]; if(i+1 == LEN) VECTOR[i-START+1] = '\0'; }
+
 
 class TATUInterpreter{
 private:
@@ -34,13 +44,11 @@ private:
         } OBJ;
         uint8_t STRUCTURE;
     } Command;
-    bool ERROR;
 public:
     Command cmd;
     uint32_t str_hash;
     TATUInterpreter(){ ERROR = true; }
     bool parse(unsigned char *, unsigned int);
-    bool getERROR();
 };
 
 #endif
